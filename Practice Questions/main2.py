ed7a62ffddb84dff490d5c0a91aed22c40f9f183
pushkar11230 <pushkarbhadani11230@gmail.com>
2026-05-24 21:12:11 +0530

    Some practice questions

---
+++ Practice Questions/main2.py
@@ -0,0 +1,91 @@
+# Count Frequency of Words
+
+# text = "python is easy and python is powerful"
+
+# frequency = {}
+
+# for word in text.split():
+#     if word in frequency:
+#         frequency[word] += 1
+#     else:
+#         frequency[word] = 1
+
+# print(frequency)
+
+
+
+
+# Find Second Largest Number
+
+# numbers = [4, 8, 1, 9, 3, 9, 7]
+
+# largest = float("-inf")
+# sec_largest = float("-inf")
+
+# for num in numbers:
+#     if num > largest:
+#         sec_largest = largest
+#         largest = num
+#     elif num < largest and num > sec_largest:
+#         sec_largest = num
+# # print("Largest:", largest)
+# print("Second largest:", sec_largest)
+
+
+
+
+
+# Check for Palindrome Sentence
+
+# text = "A man a plan a canal Panama"
+
+# n_text = text.lower().replace(" ", "")
+# reversed_text = n_text[::-1]
+
+# print(text)
+
+# if n_text == reversed_text:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+
+
+
+
+# Group Even and Odd Numbers
+
+# numbers = [1, 2, 3, 4, 5, 6]
+
+# even_odd = {
+#     "Even": [],
+#     "Odd": []
+# }
+
+# for num in numbers:
+#     if num % 2 == 0:
+#         even_odd["Even"].append(num)
+#     else:
+#         even_odd["Odd"].append(num)
+
+# print(even_odd)
+
+
+
+
+# Find Missing Numbers
+
+numbers = [1, 2, 3, 5, 6, 8, 10, 11, 12, 14]
+
+miss_nums = []
+
+for num in range(1, numbers[-1]):
+    if num not in numbers:
+        miss_nums.append(num)
+
+print(miss_nums)
+
+
+
+
+# Remove Duplicate Characters From String
+
