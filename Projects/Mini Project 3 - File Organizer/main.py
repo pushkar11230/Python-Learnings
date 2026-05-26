0982f29cd97d292c722c835b6281400b4b4464bd
pushkar11230 <pushkarbhadani11230@gmail.com>
2026-05-26 19:38:04 +0530

    Updated File organizer code

--- Projects/Mini Project 3 - File Organizer/main.py
+++ Projects/Mini Project 3 - File Organizer/main.py
@@ -1,40 +1,28 @@
 import os
 import shutil
 
-# Folder path you want to organize
-FOLDER_PATH = os.getcwd() # Current working directory
-
-# File type mapping
+FOLDER_PATH = os.getcwd()
 
 FILE_TYPES = {
-    'Images': ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp'],
-    'Documents': ['.pdf', '.docx', '.txt', '.xlsx', '.pptx'],
-    'Audio': ['.mp3', '.wav', '.aac', '.flac'],
-    'Videos': ['.mp4', '.avi', '.mkv', '.mov'],
-    'Archives': ['.zip', '.rar', '.tar', '.gz']
+    "PDFs": [".pdf"],
+    "Word_Files": [".docx", ".doc"],
+    "Excel_Files": [".xlsx", ".xls", ".csv"],
+    "Images": [".jpg", ".jpeg", ".png", ".webp", ".gif"],
+    "Videos": [".mp4", ".mkv"],
+    "Document": [".txt", ".pdf"]
 }
 
-# Create folders if they don't exist
-for folder in FILE_TYPES.keys(): # Create folders for each file type category (Which is here is the key of the FILE_TYPES dictionary)
-    folder_path = os.path.join(FOLDER_PATH, folder)
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
+for folder in FILE_TYPES:
+    if not os.path.exists(folder):
+        os.mkdir(folder)
 
-for file_name in os.listdir(FOLDER_PATH):  # Loop through each file in the folder
-    file_path = os.path.join(FOLDER_PATH, file_name)
-
-    # Skip folders and the script itself
-    if os.path.isdir(file_path):
-        continue
-    if file_name == os.path.basename(__file__):
+for file in os.listdir(FOLDER_PATH):
+    if os.path.isdir(file):
         continue
 
-    # Get file extension
-    file_extension = os.path.splitext(file_path)[1].lower()
+    ext = os.path.splitext(file)[1].lower()
 
     for folder, extensions in FILE_TYPES.items():
-        if file_extension in extensions:
-            shutil.move(file_path, os.path.join(FOLDER_PATH, folder))
+        if ext in extensions:
+            shutil.move(file, os.path.join(folder, file))
             break
-
-print("Files have been organized successfully ✅")
