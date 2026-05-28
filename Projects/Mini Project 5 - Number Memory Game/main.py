6bbc6a75c0a2acd0b60c7e05cc04f44270b0758e
pushkar11230 <pushkarbhadani11230@gmail.com>
2026-05-28 17:23:43 +0530

    Updated the replay system for Number Memory Game project

--- Projects/Mini Project 5 - Number Memory Game/main.py
+++ Projects/Mini Project 5 - Number Memory Game/main.py
@@ -19,74 +19,73 @@ import os
 import time
 
 def main_code():
-    # Instructions
-    print("You will see a random number for few seconds.")
-    print("You have to remember that number and then enter what you remembered.")
-    input("Press Enter when you are ready: \n")
+    while True:
+        # Instructions
+        print("You will see a random number for few seconds.")
+        print("You have to remember that number and then enter what you remembered.")
+        input("Press Enter when you are ready: \n")
 
-    # difficulty levels
-    difficulties = {
-        "Level 1: Easy -> 3 digits number": [100, 999],
-        "Level 2: Medium -> 4 digits number": [1000, 9999],
-        "Level 3: Hard -> 5 digits number": [10000, 99999],
-        "Level 4: Very Hard -> 6 digits number": [100000, 999999],
-        "Level 5: Pro -> 7 digits number": [1000000, 9999999],
-        "Level 6: Extreme -> 8 digits number": [10000000, 99999999]
-    }
+        # difficulty levels
+        difficulties = {
+            "Level 1: Easy -> 3 digits number": [100, 999],
+            "Level 2: Medium -> 4 digits number": [1000, 9999],
+            "Level 3: Hard -> 5 digits number": [10000, 99999],
+            "Level 4: Very Hard -> 6 digits number": [100000, 999999],
+            "Level 5: Pro -> 7 digits number": [1000000, 9999999],
+            "Level 6: Extreme -> 8 digits number": [10000000, 99999999]
+        }
 
-    # score counting
-    score = 0
-    
-    for level in difficulties:
-        # showing the round
-        print(level)
-        time.sleep(2)
+        # score counting
+        score = 0
         
-        limit = difficulties[level]
+        for level in difficulties:
+            # showing the round
+            print(level)
+            time.sleep(2)
+            
+            limit = difficulties[level]
 
-        # choosing a random number
-        rand_num = random.randint(limit[0], limit[1])
+            # choosing a random number
+            rand_num = random.randint(limit[0], limit[1])
 
-        # printing the random number only for few seconds
-        print(rand_num)
-        time.sleep(3)
-        os.system("cls")
+            # printing the random number only for few seconds
+            print(rand_num)
+            time.sleep(3)
+            os.system("cls")
+            
+            # taking user input
+            try:
+                user_input = int(input("Enter the number: "))
+            except ValueError:
+                print("Error! You did not entered a number.")
+                break
+
+            # checking the number
+            if rand_num == user_input:
+                print("Correct")
+                score += 10
+                if level != "Level 6: Extreme -> 8 digits number":
+                    input("\nPress Enter to go in next round: \n")
+                    os.system("cls")
+            else:
+                print("Wrong!")
+                print(f"The number was: {rand_num}")
+                print(f"You entered: {user_input}")
+                break
         
-        # taking user input
-        try:
-            user_input = int(input("Enter the number: "))
-        except ValueError:
-            print("Error! You did not entered a number.")
-            break
+        # showing the final score
+        print(f"\nYour score: {score}")
 
-        # checking the number
-        if rand_num == user_input:
-            print("Correct")
-            score += 10
-            if level != "Level 6: Extreme -> 8 digits number":
-                input("\nPress Enter to go in next round: \n")
-                os.system("cls")
+        # replay system
+        print("\nDo you want to play again?")
+        play = input("Type 'y' for Yes and 'n' for No: ").lower()
+        if play == "y":
+            os.system("cls")
+            continue
         else:
-            print("Wrong!")
-            print(f"The number was: {rand_num}")
-            print(f"You entered: {user_input}")
+            print("Thanks for playing...")
+            input()
             break
+        
 
-    print(f"\nYour score: {score}")
-
-def replay_system():
-    print("Do you want to play again?")
-    play = input("Type 'y' for Yes and 'n' for No: ").lower()
-
-    if play == "y":
-        os.system("cls")
-        game_code()
-    elif play == "n":
-        print("Thanks for playing...")
-        input()
-
-def game_code():
-    main_code()
-    replay_system()
-
-game_code()
+main_code()
